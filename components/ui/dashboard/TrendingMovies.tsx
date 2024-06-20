12d8bda26fcfd6fb6eb9de994d6665992683cb4d
suman <yes.u.man7@gmail.com>
2024-06-20 15:44:20 +0545

    trending movies color changed

--- components/ui/dashboard/TrendingMovies.tsx
+++ components/ui/dashboard/TrendingMovies.tsx
@@ -5,7 +5,7 @@ import { CardSkeleton } from "../Skeletons";
 const TrendingMovies = () => {
   return (
     <div>
-      <div className="max-w-7xl mx-auto ">
+      <div className="max-w-7xl mx-auto text-white">
         <div className="flex ">
           <h1 className=" max-w-7xl text-3xl font-bold my-5 ">Trending</h1>
         </div>
